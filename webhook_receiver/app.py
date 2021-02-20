db15a5d1c28be314e7c77732f01a3759622f546d
james <jamesearl341@gmail.com>
2021-02-20 16:31:16 +0000

    updates

--- webhook_receiver/app.py
+++ webhook_receiver/app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route('/docker-webhook/05b6053c41a2130afd6fc3b158bda4e6', methods=['POST', 'GET'])
 def respond():
     print(json.dumps(request.json))
+
+    try:
+        j = json.loads(request.json)
+
+        if "repository" in j:
+            if "repo_name" in j['repository']:
+                repo_name = j['repository']
+                print(repo_name)
+    
+    except Exception as e:
+        print(e)
+
     return Response(status=200)
 
 app.run(host='0.0.0.0', port=9000)
