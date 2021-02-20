e690e7388a1cfe54ca0058b9c2104f1f0ba14710
james <jamesearl341@gmail.com>
2021-02-20 16:00:28 +0000

    updates

--- webhook_receiver/app.py
+++ webhook_receiver/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/docker-webhook/05b6053c41a2130afd6fc3b158bda4e6', methods=['POST'])
+@app.route('/docker-webhook/05b6053c41a2130afd6fc3b158bda4e6', methods=['POST', 'GET'])
 def respond():
     print(request.json);
     return Response(status=200)
