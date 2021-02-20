41a5335ef8d80e5b53e7e691f70facd4f278e69e
james <jamesearl341@gmail.com>
2021-02-20 17:44:09 +0000

    updates

--- webhook_receiver/app.py
+++ webhook_receiver/app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, Response
 import json
+import pprint
+import subprocess
+import sys
+import os
 
+DIR = os.path.dirname(os.path.realpath(__file__))
+ANSIBLE_KEY = ""
 app = Flask(__name__)
 
 @app.route('/docker-webhook/05b6053c41a2130afd6fc3b158bda4e6', methods=['POST', 'GET'])
@@ -22,8 +28,28 @@ def respond():
 
     return Response(status=200)
 
-app.run(host='0.0.0.0', port=9000)
+if __name__ == "__main__":
+    ANSIBLE_KEY = int(sys.argv[1])
+    print(ANSIBLE_KEY)
+    
+    redeploy()
+    # app.run(host='0.0.0.0', port=9000)
 
 
 def redeploy():
-    pass
+    hosts = DIR + "../ansible/kube-cluster/hosts/hosts"
+    playbook = DIR + "../ansible/kube-cluster/monitoring/check-kube-cluster-health.yml"
+
+    cmd = ["ansible-playbook",
+           "-i {},".format(hosts),
+           "-e ANSIBLE_HOST_KEY_CHECKING=False",
+           playbook,
+           "-k {}".format(ANSIBLE_KEY)]
+
+    proc = subprocess.Popen(cmd,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            )
+
+    outs, errs = proc.communicate(timeout=15)
+    pprint.pprint(outs.decode().split('\n'))
