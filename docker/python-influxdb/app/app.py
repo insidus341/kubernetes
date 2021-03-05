82f59aee4cb0ca60877c0b3c4faefd31712d3e95
james <jamesearl341@gmail.com>
2021-03-05 19:43:09 +0000

    updates

--- docker/python-influxdb/app/app.py
+++ docker/python-influxdb/app/app.py
@@ -1,4 +1,71 @@
+from influxdb import InfluxDBClient
+from pprint import pprint
+import json
+import datetime
+import random
+import socket
+import subprocess
+import re
 import time
 
-time.sleep(120)
-exit(1)
+HOST="192.168.10.24"
+PORT=32665
+DATABASE="pythontest22"
+HOSTNAME=socket.gethostname()
+PING_WAIT=2
+PING_DST="8.8.8.8"
+
+def send_ping():
+    regex = "time=[0-9.]{1,6}ms"
+    
+    latency = None
+    loss = None
+
+    try:
+        cmd = "ping -W {} -c {} {}".format(PING_WAIT, 1, PING_DST).split(' ')
+        output = subprocess.check_output(cmd).decode().strip()
+        lines = output.split("\n")
+        
+        for line in lines: 
+            hit = re.search(regex, line)
+            if hit:
+                data = re.findall(regex, line)
+                latency = re.findall("[0-9]{1,6}", data[0])[0]
+                loss = 0
+
+    except Exception as e:
+        latency = 0
+        loss = 1
+
+    print("latency: {}\nloss: {}".format(latency, loss))
+    return (latency, loss)
+
+
+def insert_data(latency, loss):
+    client = InfluxDBClient(host=HOST, port=PORT)
+
+    if DATABASE not in client.get_list_database():
+        client.create_database(DATABASE)
+
+    client.switch_database(DATABASE)
+    data = [
+        {
+            "measurement": "ping_latency",
+            "tags": {
+                "host": HOSTNAME,
+            },
+            "time": datetime.datetime.now().isoformat(),
+            "fields": {
+                "latency": int(latency),
+                "loss": int(loss)
+            }
+        }
+    ]
+
+    client.write_points(data)
+
+while True:
+    (latency, loss) = send_ping()
+    insert_data(latency, loss)
+
+    time.sleep(1)
